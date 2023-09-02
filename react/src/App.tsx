862168a973f7729409fdd423e28c98e8d6c6ea67
wolfman0719 <hsato0719@icloud.com>
2023-09-02 22:45:52 +0900

    Add files via upload

---
+++ react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Pm } from './components/Pm';
+
+function App() {
+  return (
+    <div className="App">
+    <Pm />
+    </div>
+  );
+}
+
+export default App;
